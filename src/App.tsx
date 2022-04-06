f9b7fc19514f167a9598f5d2454754aca54af451
swan-prince <77962479+swan-prince@users.noreply.github.com>
2022-04-06 23:11:41 +0800

    test welcome

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from 'views/Login'
 import ResetPassword from 'views/ResetPassword'
-import Requests from 'views/Requests'
-import Statistics from 'views/Statistics'
-import Profile from 'views/Profile'
-import Clients from 'views/Clients'
-import EditOffer from 'views/EditOffer'
-import AddOffer from 'views/AddOffer'
+// import Requests from 'views/Requests'
+// import Statistics from 'views/Statistics'
+// import Profile from 'views/Profile'
+// import Clients from 'views/Clients'
+// import EditOffer from 'views/EditOffer'
+// import AddOffer from 'views/AddOffer'
 import Dashboard from 'views/Dashboard'
-import RequestDetail from 'views/RequestDetail'
-import ClientDetail from 'views/ClientDetail'
+// import RequestDetail from 'views/RequestDetail'
+// import ClientDetail from 'views/ClientDetail'
 
 function App() {
   return (
@@ -19,14 +19,14 @@ function App() {
       <Routes>
         <Route path='/auth/login' element={<Login />} />
         <Route path='/auth/reset-password' element={<ResetPassword />} />
-        <Route path='/admin/requests/:id' element={<RequestDetail />} />
+        {/* <Route path='/admin/requests/:id' element={<RequestDetail />} />
         <Route path="/admin/requests" element={<Requests />} />
         <Route path="/admin/statistics" element={<Statistics />} />
         <Route path="/admin/profile" element={<Profile />} />
         <Route path='/admin/clients/:id' element={<ClientDetail />} />
         <Route path='/admin/clients' element={<Clients />} />
         <Route path='/admin/edit-offer' element={<EditOffer />} />
-        <Route path='/admin/add-offer' element={<AddOffer />} />
+        <Route path='/admin/add-offer' element={<AddOffer />} /> */}
         <Route path="/admin/welcome" element={<Dashboard />} />
         <Route path='/*' element={<Login />} />
       </Routes>
