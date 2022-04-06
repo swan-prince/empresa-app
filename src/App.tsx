35091ae6dc052bd2713533cc470da0f46531501c
swan-prince <77962479+swan-prince@users.noreply.github.com>
2022-04-06 22:09:05 +0800

    test with login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import MainLayout from 'components/MainLayout'
-import AuthLayout from 'components/AuthLayout'
+// import MainLayout from 'components/MainLayout'
+// import AuthLayout from 'components/AuthLayout'
+
+import Login from 'views/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>       
-        <Route path="/auth/*" element={<AuthLayout />} />
-        <Route path='/admin/*' element={<MainLayout />} />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        {/* <Route path="/auth/*" element={<AuthLayout />} />
+        <Route path='/admin/*' element={<MainLayout />} /> */}
         {/* <Route path='/*' element={<AuthLayout />} />  */}
       </Routes>
     </BrowserRouter>
